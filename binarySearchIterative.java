eab9c6bbd171e47ed764cc6b5fc3577bb0995d21
v45uBansal <103349220+v45uBansal@users.noreply.github.com>
2022-04-10 10:42:45 +0530

    Add files via upload

---
+++ binarySearchIterative.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class binarySearchIterative
+{
+    public static int search(int key, ArrayList<Integer>list)
+    {
+        int low=0;
+        int high=list.size()-1;
+        while(high>=low)
+        {
+            int mid=low+(high-low)/2;
+            if(list.get(mid)==key)
+                return mid+1;
+            else if(list.get(mid)<key)
+                low=mid+1;
+            else
+                high=mid-1;         
+        }
+        return -1;
+    }
+    public static void main(String args[])
+    {
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+
+        int key=70;
+    
+        int ans=search(key,list);
+        if(ans==-1)
+          System.out.println("NOT FOUND");
+        else 
+          System.out.println("FOUND AT: "+ ans); 
+        
+    }
+}
