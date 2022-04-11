f8093d15028d25a5656bff106e90d894bcca2681
v45uBansal <103349220+v45uBansal@users.noreply.github.com>
2022-04-11 19:05:49 +0530

    Update searchInSortedrotatedArray.java

--- searchInSortedrotatedArray.java
+++ searchInSortedrotatedArray.java
@@ -2,38 +2,39 @@ import java.util.*;
 // anuj bhaiya
 // first we check whether the middle element is it or not, if not we find which part of the array is sorted and check if the element is present in the sorted part by bounds check
 // if one part is sorted then other will be sorted because in rotations we are basically shifting the two sorted parts
-class searchInSortedrotatedArray
-{
-    public static int search(int key, ArrayList<Integer>list)
-    {
+class Solution {
+    public int search(int[] nums, int target) {
         int low=0;
-        int high=list.size()-1;
+        int high=nums.length-1;
         while(high>=low)
         {
             int mid=low+(high-low)/2;
             
-            if(list.get(mid)==key)
+            if(nums[mid]== target)
                 return mid;
-            //which part is sorted
-            if(list.get(low)<list.get(mid))
+            
+            //check for the sorted part
+            
+            //left part sorted
+            else if(nums[mid]>=nums[low])
             {
-                //left part is sorted
-                if(list.get(low)<=key && list.get(mid)>key)
+                //checking range
+                if(target>=nums[low] && target<nums[mid])
                     high=mid-1;
                 else
                     low=mid+1;
             }
             else
             {
-                if(key>list.get(mid) && key<=list.get(high))
+                if(target>nums[mid] && target<=nums[high])
                     low=mid+1;
                 else
                     high=mid-1;
             }
-            
         }
         return -1;
     }
+
     public static void main(String args[])
     {
         ArrayList<Integer>list= new ArrayList<Integer>();
